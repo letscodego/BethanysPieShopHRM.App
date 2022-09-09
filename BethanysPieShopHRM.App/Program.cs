6dc7746478e6a6d765a6bdb8415fed2bd3b7c8d1
K1 <searchfordeveloping@gmail.com>
2022-09-08 22:53:40 -0700

    replaced httpclient with httpclient factory

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -1,11 +1,18 @@
 using BethanysPieShopHRM.App;
+using BethanysPieShopHRM.App.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+var apiBaseAddress = builder.Configuration["ApiBaseAddress"];
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//to be able to use the code for both blazor client-side or server-side, we need to use http factory instead of httpclient
+//builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44338") });
+
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri(apiBaseAddress));
+builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri(apiBaseAddress));
+builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client => client.BaseAddress = new Uri(apiBaseAddress));
 
 await builder.Build().RunAsync();
